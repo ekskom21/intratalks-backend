eeb780e6e45e7b89fa4c0be2cb0327ff8dfe1ecf
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-12-17 14:58:28 +0100

    Try to fix linting

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,27 @@
-const path = require("path");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        exclude: [path.resolve(__dirname, "node_modules")],
-        test: /\.ts$/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: "graphql-tag/loader",
-      },
-    ],
-  },
-  output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  target: "node",
+    module: {
+        rules: [
+            {
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader',
+            },
+        ],
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    target: 'node',
 };
