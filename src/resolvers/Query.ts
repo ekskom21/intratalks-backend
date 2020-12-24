40ba0b92821913b15d624aa22fb24074922892e6
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-24 13:55:24 +0100

    Implement resolver for companies

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,5 +1,11 @@
-import {} from '../generated/graphql';
+import { Company as CompanyT } from '../generated/graphql';
+import { Company } from '../mongoose/models';
+import { Document } from 'mongoose';
 
 export default {
-    Query: {},
+    Query: {
+        companies: async (): Promise<Array<Document<CompanyT>>> => {
+            return await Company.find({});
+        },
+    },
 };
