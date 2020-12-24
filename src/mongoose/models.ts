71e6d2e2e1e517471f50fc68ee37bf74dcd8ac53
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-12-24 15:03:02 +0100

    Only use Company model

--- src/mongoose/models.ts
+++ src/mongoose/models.ts
@@ -1,5 +1,4 @@
 import mongoose, { Document } from 'mongoose';
 import { companySchema } from './schemas';
-import { Company as CompanyT } from '../generated/graphql';
 
-export const Company = mongoose.model<Document<CompanyT>>('Company', companySchema);
+export const Company = mongoose.model<Document>('Company', companySchema);
