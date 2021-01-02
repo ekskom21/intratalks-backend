bd763454b2adc7cf9b724d47987899206859b680
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-01-02 18:52:38 +0100

    Expect both id and access token in HTTP headers

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,12 @@ import * as mutationTypeDef from './typedefs/Mutation.graphql';
 import mongoose, { Document, Model } from 'mongoose';
 import { Company } from './mongoose/models';
 
+import { verify } from 'jsonwebtoken';
+import { Claims } from './utils/jwtClaims';
+
 export type ResolverContext = {
     models: Record<'Company', Model<Document>>;
-    user?: { access_token: string };
+    user?: { access_token: string; claims: Claims };
 };
 
 (async () => {
@@ -36,13 +39,24 @@ export type ResolverContext = {
         typeDefs: [scalarTypeDef, queryTypeDef, mutationTypeDef],
         playground: environment.apollo.playground,
         context: ({ req }): ResolverContext => {
+            // access token to OW
             const access_token = req.headers.authorization;
+            // identity token for local authentication
+            const id_token = req.headers['x-user-id'] as string;
 
             let user: ResolverContext['user'] = undefined;
 
             // Check that there are two parts of the header -- "Bearer" + token
-            if (access_token && access_token.split(' ').length === 2) {
-                user = { access_token: access_token.split(' ')[1] };
+            if (access_token && access_token.split(' ').length === 2 && id_token) {
+                try {
+                    // We force unwrap as we _want_ the server to crash if the developer didn't include the public key
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    const claims = verify(id_token, process.env.PUBLIC_KEY!) as Claims;
+
+                    user = { access_token: access_token.split(' ')[1], claims };
+                } catch (e) {
+                    console.log('Invalid token supplied.');
+                }
             }
 
             return {
