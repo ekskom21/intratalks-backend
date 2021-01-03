cea1b2720b816e238c18243b3179bd0867f4c4b1
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-01-03 17:48:30 +0100

    Remove ? from adminlist

--- src/utils/adminOnly.ts
+++ src/utils/adminOnly.ts
@@ -5,7 +5,7 @@ export default (context: ResolverContext): void => {
     const adminList = process.env.ADMIN_IDS?.split(',') ?? [];
     const userID = context.user?.claims.sub || '';
 
-    if (!adminList?.includes(userID) && !environment.apollo.mockEntireSchema) {
+    if (!adminList.includes(userID) && !environment.apollo.mockEntireSchema) {
         throw Error('This query/mutation requires you to be an admin.');
     }
 };
