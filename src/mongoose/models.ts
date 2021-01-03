da019157a6b53d4ece05eb4d5b3c576c2a30f0b6
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-01-03 17:00:29 +0100

    Update plural name for assigned collection

--- src/mongoose/models.ts
+++ src/mongoose/models.ts
@@ -4,4 +4,4 @@ import { companySchema, eventSchema, eventDaySchema } from './schemas';
 export const Event = mongoose.model<Document>('Event', eventSchema);
 export const Company = mongoose.model<Document>('Company', companySchema);
 export const Interest = mongoose.model<Document>('Interest', eventDaySchema);
-export const Assigned = mongoose.model<Document>('Assigned', eventDaySchema);
+export const Assigned = mongoose.model<Document>('Assigned', eventDaySchema, 'assigned'); // plural
