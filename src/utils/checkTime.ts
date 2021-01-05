958b1dbb471bbfe5e042ce2f537b28270a4ec59a
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-01-05 18:29:59 +0100

    Change to date-fns check

--- src/utils/checkTime.ts
+++ src/utils/checkTime.ts
@@ -1,12 +1,18 @@
+import { isWithinInterval } from 'date-fns';
+
 // Set Tech Talks date to February 18th 2021 at 10 o'clock
-const techTalks = new Date(2021, 1, 18, 10);
-const MILLISECONDS_IN_TWENTY_FOUR_HOURS = 1000 * 60 * 24;
+const techTalksStart = new Date(2021, 1, 18, 10);
+
+// Set the registration deadline to February 17th 2021 at 10 o'clock
+const registationDeadline = new Date(2021, 1, 17, 10);
 
 export default (): void => {
     const now = new Date();
-    const timeDiff = techTalks.getTime() - now.getTime();
 
-    if (timeDiff < MILLISECONDS_IN_TWENTY_FOUR_HOURS) {
+    if (
+        !isWithinInterval(now, { start: registationDeadline, end: techTalksStart }) &&
+        process.env.NODE_ENV !== 'development'
+    ) {
         throw Error('The deadline for registering interest was due 24 hours before Tech Talks.');
     }
 };
