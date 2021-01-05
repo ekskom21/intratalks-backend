676df6dfae5164d783b3288f7d418b15dd190bc5
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-01-05 18:55:48 +0100

    Shorten function

--- src/utils/checkTime.ts
+++ src/utils/checkTime.ts
@@ -1,12 +1,10 @@
-import { isAfter } from 'date-fns';
+import { isPast } from 'date-fns';
 
 // Set the registration deadline to February 17th 2021 at 10 o'clock
 const registationDeadline = new Date(2021, 1, 17, 10);
 
 export default (): void => {
-    const now = new Date();
-
-    if (isAfter(now, registationDeadline) && process.env.NODE_ENV !== 'development') {
+    if (isPast(registationDeadline) && process.env.NODE_ENV !== 'development') {
         throw Error('The deadline for registering interest was due 24 hours before Tech Talks.');
     }
 };
