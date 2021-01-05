e43e38b7fc7320d59900f668bf461f423ef4bcde
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-01-05 18:34:06 +0100

    Fix logic

--- src/utils/checkTime.ts
+++ src/utils/checkTime.ts
@@ -1,7 +1,4 @@
-import { isWithinInterval } from 'date-fns';
-
-// Set Tech Talks date to February 18th 2021 at 10 o'clock
-const techTalksStart = new Date(2021, 1, 18, 10);
+import { isAfter } from 'date-fns';
 
 // Set the registration deadline to February 17th 2021 at 10 o'clock
 const registationDeadline = new Date(2021, 1, 17, 10);
@@ -9,10 +6,7 @@ const registationDeadline = new Date(2021, 1, 17, 10);
 export default (): void => {
     const now = new Date();
 
-    if (
-        !isWithinInterval(now, { start: registationDeadline, end: techTalksStart }) &&
-        process.env.NODE_ENV !== 'development'
-    ) {
+    if (isAfter(now, registationDeadline) && process.env.NODE_ENV !== 'development') {
         throw Error('The deadline for registering interest was due 24 hours before Tech Talks.');
     }
 };
